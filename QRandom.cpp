fda2355692b097970a1d3378e5c798ce43f6af72
QinZhixiang <qinzhixiang@qq.com>
2020-07-25 17:20:08 +0800

    modify Qrandom

--- QRandom.cpp
+++ QRandom.cpp
@@ -1,6 +1,10 @@
 #include "QRandom.h"
 
 int getRandNum(int min, int max) {
-    //srand(time(NULL));
+    static bool init_=false;
+    if(!init_){
+        srand(time(NULL));
+        init_=true;
+    }
     return (min + rand() % (max - min + 1));
 }
